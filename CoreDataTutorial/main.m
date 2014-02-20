6ac8f131c6aa57558cde1abb5c8ed29be153e861
Alex <alex.kovalskiy@icloud.com>
2014-02-20 22:19:33 +0200

    Initial Commit

---
+++ CoreDataTutorial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataTutorial
+//
+//  Created by Alex on 2/20/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
